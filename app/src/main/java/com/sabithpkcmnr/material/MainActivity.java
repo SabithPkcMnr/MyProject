55220f24670486126dac3520c74a66652e6e9492
Sabith Pkc Mnr <sabithpkcmnr@gmail.com>
2017-09-02 15:30:53 +0530

    Add message to display over a folder or file in GitHub file listing.

---
+++ app/src/main/java/com/sabithpkcmnr/material/MainActivity.java
@@ -0,0 +1,44 @@
+package com.sabithpkcmnr.material;
+
+
+
+
+
+
+
+
+
+
+
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        this.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        //this.getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_close_black_24dp);
+
+
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                // app icon in action bar clicked; go home
+                Intent intent = new Intent(this, MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
